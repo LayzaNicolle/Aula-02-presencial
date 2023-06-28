43d8ec9b45db8d1c03f7fbe5aa1cec557fdc82df
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-28 14:52:20 -0300

    Atividade 1, questão 02

---
+++ Atividade 1, quest 02.js
@@ -0,0 +1,9 @@
+//quest 02
+
+var numero = new Number(8000);
+console.log(numero);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.EPSILON);
+console.log(Number.MAX_SAFE_INTEGER);
